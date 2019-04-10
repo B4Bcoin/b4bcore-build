1be6ab52bc1cfad42a7c36d0cf7436bb709a5dfa
dev <dev@Ivos-MacBook-Pro.local>
2019-04-10 12:07:33 +0200

    updated for B4Bcoin

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ravencore-build
+# b4bcore-build
 
 A helper to add tasks to gulp.
 
@@ -7,16 +7,16 @@ A helper to add tasks to gulp.
 Install with:
 
 ```sh
-npm install ravencore-build
+npm install b4bcore-build
 ```
 
 and use and require in your gulp file:
 
 ```javascript
 var gulp = require('gulp');
-var ravencoreTasks = require('ravencore-build');
+var b4bcoreTasks = require('b4bcore-build');
 
-ravencoreTasks('submodule');
+b4bcoreTasks('submodule');
 gulp.task('default', ['lint', 'test', 'browser', 'coverage']);
 ```
 
@@ -25,15 +25,14 @@ gulp.task('default', ['lint', 'test', 'browser', 'coverage']);
 * There's no default task to allow for each submodule to set up their own configuration
 * If the module is node-only, avoid adding the browser tasks with:
 ```javascript
-var ravencoreTasks = require('ravencore-build');
-ravencoreTasks('submodule', {skipBrowsers: true});
+var b4bcoreTasks = require('b4bcore-build');
+b4bcoreTasks('submodule', {skipBrowsers: true});
 ```
 
 ## Contributing
 
-See [CONTRIBUTING.md](https://github.com/RavenDevKit/ravencore) on the main ravencore repo for information about how to contribute.
+See [CONTRIBUTING.md](https://github.com/B4Bcoin/b4bcore) on the main b4bcore repo for information about how to contribute.
 
 ## License
 
-Code released under [the MIT license](https://github.com/RavenDevKit/ravencore/blob/master/LICENSE).
-
+Code released under [the MIT license](https://github.com/B4Bcoin/b4bcore/blob/master/LICENSE).
--- index.js
+++ index.js
@@ -17,8 +17,8 @@
  * </ul>`
  * <li> `browser` - generate files needed for browser (browserify)
  * <ul>
- * <li> `browser:uncompressed` - build uncomprssed browser bundle (`ravencore-*.js`)
- * <li> `browser:compressed` - build compressed browser bundle (`ravencore-*.min.js`)
+ * <li> `browser:uncompressed` - build uncomprssed browser bundle (`b4bcore-*.js`)
+ * <li> `browser:compressed` - build compressed browser bundle (`b4bcore-*.min.js`)
  * <li> `browser:maketests` - build `tests.js`, needed for testing without karma
  * </ul>`
  * <li> `lint` - run `jshint`
@@ -54,12 +54,12 @@ function startGulp(name, opts) {
 
   opts = opts || {};
   var browser = !opts.skipBrowser;
-  var fullname = name ? 'ravencore-' + name : 'ravencore';
+  var fullname = name ? 'b4bcore-' + name : 'b4bcore';
   var files = ['lib/**/*.js'];
   var tests = ['test/**/*.js'];
   var alljs = files.concat(tests);
 
-  var buildPath = './node_modules/ravencore-build/';
+  var buildPath = './node_modules/b4bcore-build/';
   var buildModulesPath = buildPath + 'node_modules/';
   var buildBinPath = buildPath + 'node_modules/.bin/';
 
@@ -129,9 +129,9 @@ function startGulp(name, opts) {
     var browserifyCommand;
 
     if (name !== 'lib') {
-      browserifyCommand = browserifyPath + ' --require ./index.js:' + fullname + ' --external ravencore-lib -o ' + fullname + '.js';
+      browserifyCommand = browserifyPath + ' --require ./index.js:' + fullname + ' --external b4bcore-lib -o ' + fullname + '.js';
     } else {
-      browserifyCommand = browserifyPath + ' --require ./index.js:ravencore-lib -o ravencore-lib.js';
+      browserifyCommand = browserifyPath + ' --require ./index.js:b4bcore-lib -o b4bcore-lib.js';
     }
 
     gulp.task('browser:uncompressed', shell.task([
